8dd7bb47b2c36e14531138321f74c06a551b59bf
Victor Calderon <calderon.j.victor@gmail.com>
2023-09-20 11:41:16 -0500

    test

--- src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
@@ -1,34 +1,54 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import edu.codeup.codeupspringblog.models.Post;
+import edu.codeup.codeupspringblog.models.User;
+import edu.codeup.codeupspringblog.repositories.PostRepository;
+import edu.codeup.codeupspringblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
+    private PostRepository postDao;
+    private UserRepository userDao;
+
+    public PostController(UserRepository userDao, PostRepository postDao1) {
+        this.userDao = userDao;
+        this.postDao = postDao1;
+    }
+
     @GetMapping("")
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    public String post(Model model) {
+        model.addAttribute("posts", postDao.findAll());
+        return "posts/index";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public String view(@PathVariable long id) {
-        return String.format("Hello, %s!", id);
+    public String view(@PathVariable long id, Model vModel) {
+        if(postDao.existsById(id)) {
+            Post post = postDao.findById(id).get();
+            vModel.addAttribute("blogpost", post);
+            return "posts/show";
+        }
+        return "redirect:/posts";
     }
-    @GetMapping("/create")
 
-    @ResponseBody
+    @GetMapping("/create")
     public String create(){
-        return "view the form for creating a post";
+        return "posts/create";
     }
 
     @PostMapping("/create")
-    @ResponseBody
-    public String createPost(){
-        return "create a new post";
+    public String createPost(@RequestParam("title") String title, @RequestParam("body") String body){
+        //Hard coded user
+        User hardCodeUser = userDao.findById(2L).get();
+
+        Post newPost = new Post(title, body, hardCodeUser);
+        postDao.save(newPost);
+        return "redirect:/posts";
     }
 
 
--- src/main/java/edu/codeup/codeupspringblog/models/Post.java
+++ src/main/java/edu/codeup/codeupspringblog/models/Post.java
@@ -1,4 +1,5 @@
 package edu.codeup.codeupspringblog.models;
+import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +10,63 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    //boiler plates
+    // Include ano so that spring knows ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(columnDefinition = "TEXT")
     private String body;
+    @Column
     private String title;
+
+    public Post(String body, String title) {
+        this.body = body;
+        this.title = title;
+    }
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Post(String body, String title, User user) {
+        this.body = body;
+        this.title = title;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
